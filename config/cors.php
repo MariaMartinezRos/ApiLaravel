ad8a28842198a860b5634ef319cc7d48d0f5c430
MariaMartinezRos <cibernuggets@gmail.com>
2024-11-10 10:34:01 +0100

    autentificacion

--- config/cors.php
+++ config/cors.php
@@ -29,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true, //para que solicite autenticacion para cuando se quiera acceder a la api
 
 ];
--- routes/api.php
+++ routes/api.php
@@ -7,17 +7,32 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware('auth:sanctum'); //middleware de autenticacion sanctum
 
 
 Route::get('lists/categories', [CategoryController::class, 'list']);
+
+Route::middleware('auth:sanctum')->group(function (){
+    Route::apiResource('categories', CategoryController::class);
+    Route::get('products', [ProductController::class, 'index']);
+});
+//cualquier ruta de la api que necesite autenticacion, solo hay que meterla dentro del grupo de las rutas
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //Route::get('categories', [CategoryController::class, 'index']);
 //Route::get('categories/{category}', [CategoryController::class, 'show']);
 //Route::post('categories', [CategoryController::class,'store']);
 //Route::put('categories/{category}', [CategoryController::class, 'update']);
 //Route::delete('categories/{category}', [CategoryController::class, 'destroy']);
-
-Route::apiResource('categories', CategoryController::class);
-
-Route::get('products', [ProductController::class, 'index']);
