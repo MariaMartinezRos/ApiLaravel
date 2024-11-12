a9058f55511dc8689dfeaf5b9b1f7d8c0b0bab76
MariaMartinezRos <cibernuggets@gmail.com>
2024-11-12 16:28:16 +0100

    paso 16 a medio

--- routes/api.php
+++ routes/api.php
@@ -7,12 +7,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum'); //middleware de autenticacion sanctum
+})->middleware('auth:api'); //middleware de autenticacion
 
 
 Route::get('lists/categories', [CategoryController::class, 'list']);
 
-Route::middleware('auth:sanctum')->group(function (){
+Route::middleware('auth:api')->group(function (){
     Route::apiResource('categories', CategoryController::class);
     Route::get('products', [ProductController::class, 'index']);
 });
