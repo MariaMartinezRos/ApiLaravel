e3f1e0b10df92e609e31994b3dfe5102deb76b2d
MariaMartinezRos <cibernuggets@gmail.com>
2024-11-19 17:14:06 +0100

    documentacion

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,14 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @oainfo(
+ *     version = "1.0.0",
+ *     title = "API IES",
+ *     description = "API para aprendizaje (IES)",
+ *     )
+ */
+
 abstract class Controller
 {
     //
